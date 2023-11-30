6f4408d03d0dda55beadf5618129398508ae1521
Jun Zhang <jzhang213@sheffield.ac.uk>
2023-11-30 02:46:03 +0000

    basic function of task 1

---
+++ TruthTable.hs
@@ -0,0 +1,72 @@
+module TruthTable where
+
+import Data.List
+import Data.Maybe
+
+-- define the boolvalue type
+data BoolValue = T | F deriving (Show, Eq)
+
+-- define data type of basic logic notations
+data LogicFormula = Var Char    -- propositional variable
+                   | Not LogicFormula
+                   | LogicFormula :/\ LogicFormula
+                   | LogicFormula :\/ LogicFormula
+                   | LogicFormula :-> LogicFormula
+                   | Bottom
+                   | Top
+                       deriving (Show, Eq)
+
+
+-- main program
+-- TEST:
+-- putStrLn(truthTable (((Var 'p') :\/ (Var 'd')) :-> ((Var 'q') :/\(Var 'r'))))
+-- putStrLn(truthTable (Var 'p' :\/ Var 'q'))
+truthTable :: LogicFormula -> String
+truthTable formula = firstRow ++ "\n" ++ intercalate "\n" [rowString formula status | status <- allPosStatus (variablesStr formula)]
+  where
+    firstRow = "0\t" ++ intercalate "\t" (map (\v -> [v]) (variablesStr formula)) ++ "\tResult"
+    rowString formula status = intercalate "\t" (map (\v -> showBool (calculator (Var v) status)) (variablesStr formula)) ++ "\t" ++ showBool (calculator formula status)
+
+
+-- Get all non-repeating propositional variables from a given formula
+-- TEST:
+-- variablesStr (Var 'p' :\/ Var 'q')
+-- variablesStr (((Var 'p') :\/ (Var 'd')) :-> ((Var 'q') :/\(Var 'r')))
+variablesStr :: LogicFormula -> [Char]
+variablesStr (Var v) = [v]      -- get propositional variable
+variablesStr (Not formula) = variablesStr formula
+variablesStr (formula1 :/\ formula2) = nub (variablesStr formula1 ++ variablesStr formula2)
+variablesStr (formula1 :\/ formula2) = nub (variablesStr formula1 ++ variablesStr formula2)
+variablesStr (formula1 :-> formula2) = nub (variablesStr formula1 ++ variablesStr formula2)
+variablesStr Bottom = []
+variablesStr Top = []
+
+
+-- Generate a nested list of all possible variable assignments
+-- TEST:
+-- allPosStatus "pd"
+allPosStatus :: [Char] -> [[(Char, BoolValue)]]
+allPosStatus [] = [[]]
+allPosStatus (v:vs) = [(v, T):status | status <- rest] ++ [(v, F):status | status <- rest]
+  where rest = allPosStatus vs
+
+
+-- calculate the bool value of given formula and case status
+-- TEST:
+-- calculator (((Var 'p') :\/ (Var 'd')) :-> ((Var 'q') :/\(Var 'r'))) [('p',T),('d',T),('q',T),('r',T)]
+calculator :: LogicFormula -> [(Char, BoolValue)] -> BoolValue
+calculator (Var v) status = fromMaybe (error ("Variable " ++ [v] ++ " not found in environment")) (lookup v status)
+calculator (Not formula) status = if calculator formula status == T then F else T
+
+calculator (formula1 :/\ formula2) status = if calculator formula1 status == T && calculator formula2 status == T then T else F
+calculator (formula1 :\/ formula2) status = if calculator formula1 status == F && calculator formula2 status == F then F else T
+calculator (formula1 :-> formula2) status = if calculator formula1 status == T && calculator formula2 status == F then F else T
+
+calculator Bottom _ = F
+calculator Top _ = T
+
+
+-- show value of Bool type to String
+showBool :: BoolValue -> String
+showBool T = "T"
+showBool F = "F"
