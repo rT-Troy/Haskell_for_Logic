91c99fe4aef4f49836864029dca6ad9cd5c0114f
Jun Zhang <jzhang213@sheffield.ac.uk>
2023-12-01 01:32:09 +0000

    test for Haddock

--- TruthTable.hs
+++ TruthTable.hs
@@ -3,10 +3,11 @@ module TruthTable where
 import Data.List
 import Data.Maybe
 
--- define the boolvalue type
+-- | define the boolvalue type
 data BoolValue = T | F deriving (Show, Eq)
 
--- define data type of basic logic notations
+-- | Defining data type of basic logic notations
+-- ¬ φ, φ ∧ ψ, φ ∨ ψ, φ → ψ, ⊥, ⊤ 
 data LogicFormula = Var Char    -- propositional variable
                    | Not LogicFormula
                    | LogicFormula :/\ LogicFormula
@@ -17,10 +18,15 @@ data LogicFormula = Var Char    -- propositional variable
                        deriving (Show, Eq)
 
 
--- main program
+-- | main program
 -- TEST:
--- putStrLn(truthTable (((Var 'p') :\/ (Var 'd')) :-> ((Var 'q') :/\(Var 'r'))))
--- putStrLn(truthTable (Var 'p' :\/ Var 'q'))
+-- >>> formula1 = ((Var 'p') :\/ (Var 'd'))
+-- >>> formula2 = ((Var 'q') :/\(Var 'r'))
+-- >>> formulaComb = ((Var 'p') :\/ (Var 'd')) :-> ((Var 'q') :/\(Var 'r'))
+-- >>> putStrLn (truthTable formulaComb)
+-- >>> putStrLn (truthTable formula1)
+-- WAS WAS WAS WAS WAS parse error (possibly incorrect indentation or mismatched brackets)
+-- WAS WAS WAS WAS NOW parse error on input `)'
 truthTable :: LogicFormula -> String
 truthTable formula = firstRow ++ "\n" ++ intercalate "\n" [rowString formula status | status <- allPosStatus (variablesStr formula)]
   where
@@ -28,10 +34,12 @@ truthTable formula = firstRow ++ "\n" ++ intercalate "\n" [rowString formula sta
     rowString formula status = intercalate "\t" (map (\v -> showBool (calculator (Var v) status)) (variablesStr formula)) ++ "\t" ++ showBool (calculator formula status)
 
 
--- Get all non-repeating propositional variables from a given formula
+-- | Get all non-repeating propositional variables from a given formula
 -- TEST:
--- variablesStr (Var 'p' :\/ Var 'q')
--- variablesStr (((Var 'p') :\/ (Var 'd')) :-> ((Var 'q') :/\(Var 'r')))
+-- >>> variablesStr (Var 'p' :\/ Var 'q')
+-- >>> variablesStr (((Var 'p') :\/ (Var 'd')) :-> ((Var 'q') :/\(Var 'r')))
+-- "pq"
+-- "pdqr"
 variablesStr :: LogicFormula -> [Char]
 variablesStr (Var v) = [v]      -- get propositional variable
 variablesStr (Not formula) = variablesStr formula
@@ -42,18 +50,18 @@ variablesStr Bottom = []
 variablesStr Top = []
 
 
--- Generate a nested list of all possible variable assignments
+-- | Generate a nested list of all possible variable assignments
 -- TEST:
--- allPosStatus "pd"
+-- >>> allPosStatus "pd"
 allPosStatus :: [Char] -> [[(Char, BoolValue)]]
 allPosStatus [] = [[]]
 allPosStatus (v:vs) = [(v, T):status | status <- rest] ++ [(v, F):status | status <- rest]
   where rest = allPosStatus vs
 
 
--- calculate the bool value of given formula and case status
+-- | calculate the bool value of given formula and case status
 -- TEST:
--- calculator (((Var 'p') :\/ (Var 'd')) :-> ((Var 'q') :/\(Var 'r'))) [('p',T),('d',T),('q',T),('r',T)]
+-- >>> calculator (((Var 'p') :\/ (Var 'd')) :-> ((Var 'q') :/\(Var 'r'))) [('p',T),('d',T),('q',T),('r',T)]
 calculator :: LogicFormula -> [(Char, BoolValue)] -> BoolValue
 calculator (Var v) status = fromMaybe (error ("Variable " ++ [v] ++ " not found in environment")) (lookup v status)
 calculator (Not formula) status = if calculator formula status == T then F else T
@@ -66,7 +74,7 @@ calculator Bottom _ = F
 calculator Top _ = T
 
 
--- show value of Bool type to String
+-- | show value of Bool type to String
 showBool :: BoolValue -> String
 showBool T = "T"
 showBool F = "F"
