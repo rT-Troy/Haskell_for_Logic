b36c2deac1e9942081acc04a0b34b9264a61fa42
Jun Zhang <jzhang213@sheffield.ac.uk>
2024-02-19 21:48:49 +0000

    calculate clause and literal with bug fix

--- src/CNF.hs
+++ src/CNF.hs
@@ -1,6 +1,6 @@
 module CNF where
 --import Data.List.Split (splitOneOf)
-
+import Data.List (nub)
 
 data BoolValue = T | F deriving (Show, Eq)
 
@@ -13,19 +13,33 @@ data LogicFormula = Var Char    -- propositional variable
                    | Top
                        deriving (Show, Eq)
 
+
+-- test: (¬p∨q∨r)∧(¬p∨r)∧¬q
+
+
 -- toClause (Var 'p' :/\ Var 'q' :/\ (Var 'r' :\/ Var 'd'))
--- toClause (Var 'p' :/\ Var 'q' :/\ (Var 'r' :\/ (Not (Var 'd'))))
+-- toClause (((Not (Var 'p')) :\/ Var 'q' :\/ Var 'r') :/\ ((Not (Var 'p')) :\/ Var 'r') :/\ (Not (Var 'q')))
 -- [Var 'p',Var 'q',Var 'r' :\/ Var 'd']
-toClause :: LogicFormula -> [LogicFormula]-- -> BoolValue
-toClause (clause1 :/\ clause2) = toClause clause1 ++ [clause2]
-toClause clause1 = [clause1]
+toClause :: LogicFormula -> [LogicFormula]
+toClause (clause1 :/\ clause2) = toClause clause1 ++ toClause clause2
+toClause clause = [clause]
+
 
 -- eachClause [Var 'p',Var 'q',Var 'r' :\/ Not (Var 'd')]
+-- eachClause [(Not (Var 'p') :\/ Var 'q') :\/ Var 'r',Not (Var 'p') :\/ Var 'r',Not (Var 'q')]
 eachClause :: [LogicFormula] -> [LogicFormula]
 eachClause [] = []
-eachClause (clause:clauses) = toLiteral clause ++ eachClause clauses
+eachClause (clause:clauses) = eachLiteral clause ++ eachClause clauses
 
--- toLiteral (Var 'r' :\/ Not (Var 'd'))
+
+-- eachLiteral (Var 'r' :\/ Not (Var 'd'))
+-- eachLiteral ((Not (Var 'p') :\/ Var 'q') :\/ Var 'r')
+eachLiteral :: LogicFormula -> [LogicFormula]
+eachLiteral (literal1 :\/ literal2) = eachLiteral literal1 ++ eachLiteral literal2
+eachLiteral literal = [literal]
+
+
+-- toLiteral (Var 'p' :/\ Var 'q' :/\ (Var 'r' :\/ Var 'd'))
+-- toLiteral (((Not (Var 'p')) :\/ Var 'q' :\/ Var 'r') :/\ ((Not (Var 'p')) :\/ Var 'r') :/\ (Not (Var 'q')))
 toLiteral :: LogicFormula -> [LogicFormula]
-toLiteral (literal1 :\/ literal2) = toLiteral literal1 ++ [literal2]
-toLiteral literal1 = [literal1]
+toLiteral formula = nub (eachClause (toClause formula))
